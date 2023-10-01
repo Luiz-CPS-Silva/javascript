566154c6178c7cc396b8db16579415e2b094d9c9
Luiz Carlos <luizx720@gmail.com>
2023-09-30 21:20:16 -0300

    Atualizando Ex.:14

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,6 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${horas} horas.`
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom Dia
+        img.src = 'foto-manha.jpg'
+        document.body.style.background = '#FBCB83'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa Tarde
+        img.src = 'foto-tarde.jpg'
+        document.body.style.background = '#F29C50'
+    } else {
+        //Boa Noite
+        img.src = 'foto-noite.jpg'
+        document.body.style.background = '#023373'
+    }
 }
 
